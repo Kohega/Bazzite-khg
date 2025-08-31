5c0c6d73ea914a563da39226ed6306de8830a1b2
Kohega <kohega@tutanota.com>
2025-08-31 14:58:24 +0200

    fix: emergency mode

--- build_files/build-initramfs.sh
+++ build_files/build-initramfs.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/bash
+set -euo pipefail
 
-set -eoux pipefail
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
 
-KERNEL_VERSION="$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)"
+log() {
+  echo "=== $* ==="
+}
 
-# Ensure Initramfs is generated
+log "Building initramfs"
+
+# Get kernel version and build initramfs
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+KERNEL_VERSION="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' kernel)"
+/usr/bin/dracut \
+  --no-hostonly \
+  --kver "$KERNEL_VERSION" \
+  --reproducible \
+  --zstd \
+  -v \
+  --add ostree \
+  -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+
+chmod 0600 "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+
+log "Build completed"
